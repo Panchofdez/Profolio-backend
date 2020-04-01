a904a05b801bb94bb61fd4376d07e1b685311f27
Pancho Fernandez <panchofern@gmail.com>
2020-04-01 16:30:08 -0400

    ready to deploy to heroku

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ app.use('/myportfolio',myPortfolioRoutes);
 app.use(userProfileRoutes);
 
 
-app.listen(3001, ()=>{
+app.listen(process.env.PORT || 3001, ()=>{
 	console.log('Server is running on port 3001...')
 })
