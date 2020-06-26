a353cf38be69f55689341b52b76bdf912c0aefdb
Pancho Fernandez <panchofern@gmail.com>
2020-06-26 00:30:37 -0400

    added facebook login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
 
 router.post('/signup', async (req,res)=>{
 	const {email, password,name} = req.body;
-	if(!email || !password || !name){
+	if(isEmpty(email) || isEmpty(password) || isEmpty(name)){
 		return res.status(422).send({error:"Email, password and name are required"});
+	}
+	if(!isEmail(email)){
+		return res.status(422).json({error:'Please provide a valid email'});
 	}
 	try{
 		const user = new User({
@@ -25,7 +29,7 @@ router.post('/signup', async (req,res)=>{
 
 router.post('/signin', async (req,res)=>{
 	const {email,password} = req.body;
-	if(!email || !password){
+	if(isEmpty(email) || isEmpty(password)){
 		return res.status(422).send({error:"Must provide email and password"});
 	}
 
@@ -45,8 +49,58 @@ router.post('/signin', async (req,res)=>{
 
 })
 
+router.post('/facebooklogin', (req, res)=>{
+	const {accessToken, userId} = req.body;
+
+	let urlGraphFacebook = `https://graph.facebook.com/v7.0/${userId}/?fields=id,name,email&access_token=${accessToken}`;
+	fetch(urlGraphFacebook,{
+		method:'GET'
+	})
+	.then(response => response.json())
+    .then(response =>{
+    	const {email, name}=response;
+    	User.findOne({email}).exec((err,user)=>{
+    		if(err){
+    			return res.status(400).json({error:'Something went wrong...'});
+    		}else{
+    			if(user){
+    				console.log(user);
+    				const token = jwt.sign({userId:user._id, name:user.name}, process.env.SECRET_KEY);
+					const notifications = user.notifications.filter((n)=>n.read===false);
+					return res.status(200).send({token, userId:user._id, name:user.name, profileImage:user.profileImage, notifications, portfolio:user.portfolio});
+    			}else{
+    				let password = email+process.env.SECRET_KEY;
+    				const user = new User({
+						email, 
+						password,
+						name
+					});
+					user.save((err, data)=>{
+						if(err){
+							return res.status(400).json({error:'Something went wrong...'});
+						}else{
+							const token = jwt.sign({userId:data._id, name},process.env.SECRET_KEY);
+							return res.status(200).send({token, userId:data._id, name});
+						}
+					});
+					
+    			}
+    		}
+    	});
+    })
+})
 
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+};
 
 
 
